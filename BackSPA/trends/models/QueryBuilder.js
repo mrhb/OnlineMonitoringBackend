bd12e9a46aa98c29016dfa047edb85aa3a5682b3
mrhb <mohammad_hajjar@yahoo.com>
2020-10-28 10:08:18 +0330

    start and end date was placed by mistake

--- BackSPA/trends/models/QueryBuilder.js
+++ BackSPA/trends/models/QueryBuilder.js
@@ -3,9 +3,11 @@ exports.concat=(metrics,start,end)=>
 
 var startDate = new Date();
 startDate.setTime(start);
+console.log("startDate: "+startDate);
 
 var endDate = new Date();
 endDate.setTime(end);
+console.log("endDate: "+endDate);
 
     // this function creat string like nex line:
    // `SELECT mean("Oil_P") FROM "ModbusLogger" WHERE time >= now() - 5m GROUP BY time(10s) fill(linear)`
