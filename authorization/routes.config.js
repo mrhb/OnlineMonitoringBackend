f88b0c826a2a87f5ab8a3003d7be5351cacee01a
mrhb <mohammad_hajjar@yahoo.com>
2020-11-24 11:58:27 +0330

    change Route

--- authorization/routes.config.js
+++ authorization/routes.config.js
@@ -1,15 +1,17 @@
 const VerifyUserMiddleware = require('./middlewares/verify.user.middleware');
 const AuthorizationController = require('./controllers/authorization.controller');
 const AuthValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
+
+const baseUrl='/auth';
 exports.routesConfig = function (app) {
 
-    app.post('/auth', [
+    app.post(baseUrl, [
         VerifyUserMiddleware.hasAuthValidFields,
         VerifyUserMiddleware.isPasswordAndUserMatch,
         AuthorizationController.login
     ]);
 
-    app.post('/auth/refresh', [
+    app.post(baseUrl+'/refresh', [
         AuthValidationMiddleware.validJWTNeeded,
         AuthValidationMiddleware.verifyRefreshBodyField,
         AuthValidationMiddleware.validRefreshNeeded,
