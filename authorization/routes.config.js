82868ba59b2774b5a87a52b457aaa7c05a7839bf
mrhb <mohammad_hajjar@yahoo.com>
2021-01-21 08:47:30 +0330

    restrict setOwnerId api with admin users

--- authorization/routes.config.js
+++ authorization/routes.config.js
@@ -1,6 +1,14 @@
 const VerifyUserMiddleware = require('./middlewares/verify.user.middleware');
 const AuthorizationController = require('./controllers/authorization.controller');
 const AuthValidationMiddleware = require('../common/middlewares/auth.validation.middleware');
+const PermissionMiddleware = require('../common/middlewares/auth.permission.middleware');
+
+const config = require('../common/config/env.config');
+
+
+const ADMIN = config.permissionLevels.ADMIN;
+const OWNER = config.permissionLevels.OWNER;
+const FREE = config.permissionLevels.NORMAL;
 
 const baseUrl='/auth';
 exports.routesConfig = function (app) {
@@ -16,6 +24,7 @@ exports.routesConfig = function (app) {
     ]);
     app.post(baseUrl+'/setOwnerId', [
         AuthValidationMiddleware.validJWTNeeded,
+        PermissionMiddleware.minimumPermissionLevelRequired(ADMIN),
         VerifyUserMiddleware.isOwnerIdValid,
         AuthorizationController.login
     ]);
